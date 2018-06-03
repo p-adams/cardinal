6f6d8e3fce450c38a3e182e5cb71a0f5f91896ca
p-adams <paadams@nmu.edu>
2018-06-03 16:21:58 -0500

    add button to load data

--- src/cardinal/core.cljs
+++ src/cardinal/core.cljs
@@ -1,22 +1,22 @@
 (ns cardinal.core
-    (:require [reagent.core :as reagent :refer [atom ]]
-              [ajax.core :refer [GET]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET POST]]))
 
 (enable-console-print!)
 
 (defonce base-url "http://numbersapi.com")
 
-(defonce app-state (atom {:fact ""}))
+(defonce app-state (atom {:fact "" :input "44" :type "math"}))
 
-(defn format-base-url [url endpoint]
-  (str url endpoint "?json"))
+(defn format-base-url [url data type]
+  (str url "/" data "/" type "?json"))
 
 (defn get-fact []
-  (GET (format-base-url base-url "/44/math") {:response-format :json
-                                              :keywords? true
-                                              :handler
-                                              (fn [response]
-                                                (swap! app-state assoc :fact (:text response)))}))
+  (GET (format-base-url base-url (:input @app-state) (:type @app-state)) {:response-format :json
+                                                                                :keywords?       true
+                                                                                :handler
+                                                                                                 (fn [response]
+                                                                                                   (swap! app-state assoc :fact (:text response)))}))
 
 (defn app-form [])
 
@@ -26,22 +26,20 @@
 (defn app-navbar [])
 
 (defn app []
-  (reagent/create-class
-  {:component-did-mount #(get-fact)
-  :reagent-render
     (fn []
       [:div
-        [:h1 "Cardinal"]
-        [:div
-          [:p "Fact about 44"]
-          [:p (:fact @app-state)]
-          (app-input)]])}))
+       [:h1 "Cardinal"]
+       [:div
+        [:p "Fact about 44"]
+        [:button {:on-click get-fact} "load fact"]
+        [:p (:fact @app-state)]
+        (app-input)]]))
 
 (reagent/render-component [app]
                           (. js/document (getElementById "app")))
 
-(defn on-js-reload []
+(defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+
